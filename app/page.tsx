b4e4daebfef6687589b6e7c8dd3523e24629d4d7
wp3527kcc <44993101+wp3527kcc@users.noreply.github.com>
2024-12-30 14:51:54 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
           <li>koko.zhong demo nextjs app</li>
+          <h3>new Text</h3>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
