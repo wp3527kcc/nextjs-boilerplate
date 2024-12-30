54f2de89014d4f8a1f89627fbab55cc8adfbb7fa
zhongke <719045449@qq.com>
2024-12-30 20:38:29 +0800

    feat: add page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Counter from './components/Count'
 
 export default function Home() {
   return (
@@ -22,6 +23,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
           <li>koko.zhong demo nextjs app123</li>
+          <Counter />
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
