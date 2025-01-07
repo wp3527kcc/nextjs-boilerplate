2adf472e19b8e7568f5f9514a34f09c907281189
zhongke <719045449@qq.com>
2025-01-07 10:23:46 +0800

    feat: 修改按钮样式

--- app/components/Count.tsx
+++ app/components/Count.tsx
@@ -1,14 +1,20 @@
 "use client"
 
-import { useState } from "react";
+import {CSSProperties, useState} from "react";
 
+const btnStyle:CSSProperties = {
+    border : "1px solid #fa541c",
+    color : "#fa541c",
+    padding : "4px 12px",
+    borderRadius: 4,
+    marginRight: 12
+}
 function Counter() {
     const [count, setCount] = useState(0)
     return (<div>
+        <button onClick={() => setCount(count + 1)} style={btnStyle}>+</button>
+        <button onClick={() => setCount(count - 1)} style={btnStyle}>-</button>
         <span>{count}</span>
-        <br />
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => setCount(count - 1)}>-</button>
     </div>)
 }
 
