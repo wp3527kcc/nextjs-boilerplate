3279b430bc23e0d3e690f4d69475be166f8a57b1
zhongke <719045449@qq.com>
2025-01-13 18:12:11 +0800

    feat: 添加ajax方式

---
+++ app/api/submit/route.ts
@@ -0,0 +1,29 @@
+import {NextRequest, NextResponse} from 'next/server'
+import {getUserList} from "@/app/action";
+
+export async function POST(request: NextRequest) {
+    try {
+        // Parse the incoming JSON body
+        const body = await request.json()
+
+        // Validate the request body
+        if (!body.name) {
+            return NextResponse.json(
+                {error: 'Name is required1'},
+                {status: 400}
+            )
+        }
+
+        // Process the request
+        return NextResponse.json(
+            {message: 'Successfully processed POST request', data: body, list: await getUserList()},
+            {status: 201}
+        )
+    } catch (error) {
+        // Handle any errors
+        return NextResponse.json(
+            {error: 'Internal Server Error'},
+            {status: 500}
+        )
+    }
+}
