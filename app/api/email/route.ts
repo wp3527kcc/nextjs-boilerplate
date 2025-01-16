496b7183b4e936cdf8666dcaba168bd9171ecb62
zhongke <719045449@qq.com>
2025-01-16 10:39:27 +0800

    feat: add log

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -7,29 +7,31 @@ function logToFeiShu(
     webhookUrl = "https://open.feishu.cn/open-apis/bot/v2/hook/ad15802e-b359-451e-931e-78af5fc8c68d",
     maxCount = 20
 ) {
+    console.log('count is=>',count)
     // 限制一下发送次数，避免无限发送
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    count++ < maxCount &&
-    fetch(webhookUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            card: {
-                elements: [
-                    {
-                        tag: "div",
-                        text: {
-                            tag: "lark_md",
-                            content,
-                        },
-                    },
-                ],
+    if (count++ < maxCount) {
+        fetch(webhookUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
             },
-            msg_type: "interactive",
-        }),
-    });
+            body: JSON.stringify({
+                card: {
+                    elements: [
+                        {
+                            tag: "div",
+                            text: {
+                                tag: "lark_md",
+                                content,
+                            },
+                        },
+                    ],
+                },
+                msg_type: "interactive",
+            }),
+        });
+    }
 }
 
 export async function GET() {
