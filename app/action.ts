e7ea1fe749d507ef4b6478c3b4af76f13177480f
zhongke <719045449@qq.com>
2025-02-06 10:48:38 +0800

    feat: add action demo

--- app/action.ts
+++ app/action.ts
@@ -1,79 +1,62 @@
-'use server'
+"use server"
 import { neon } from "@neondatabase/serverless"; // Your database client
 import { put } from "@vercel/blob";
-import { Redis } from '@upstash/redis'
-// import {
-//     // cookies,
-//     headers
-// } from 'next/headers'
+import { Redis } from "@upstash/redis"
 
-const sql = neon(`${process.env.DATABASE_URL}`);
+const sql = neon("" + process.env.DATABASE_URL);
 const redis = new Redis({
-    url: 'https://brief-kid-53738.upstash.io',
+    url: "https://brief-kid-53738.upstash.io",
     token: process.env.KV_REST_API_TOKEN,
 })
+
 // const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 
 export async function fetchUsers(key: string) {
-    const data = await sql('SELECT * FROM users') as { id: string, name: string, email: string }[];
+    const data = await sql("SELECT * FROM users") as { id: string, name: string, email: string }[];
     return [data, key]
 }
 
 export async function fetchComents() {
-    const data = await sql('SELECT * FROM comments');
+    const data = await sql("SELECT * FROM comments");
     return data as { comment: string }[]
 }
 
 export async function createComment(comment: string) {
     // Connect to the Neon database
     // Insert the comment from the form into the Postgres database
-    const [, fileUrl] = await Promise.all([sql('INSERT INTO comments (comment) VALUES ($1)', [comment]), putFile(comment)]);
+    const [, fileUrl] = await Promise.all([sql("INSERT INTO comments (comment) VALUES ($1)", [comment]), putFile(comment)]);
     return fileUrl
 }
 
 // export async function deleteComment(comment: string) {
 // }
 
-export async function putFile(comment: string) {
-    const { url } = await put('articles/blob.txt', comment, { access: 'public' });
+async function putFile(comment: string) {
+    const { url } = await put("articles/blob.txt", comment, { access: "public" });
     return url
 }
 
-// const redisHeaders = new Headers({
-//     Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`
-// })
-
 export async function syncRedis(key: string, isAdd = false) {
-    // const resp = await fetch(`https://brief-kid-53738.upstash.io/${isAdd ? 'incr' : 'decr'}/${key}`, {
-    //     headers: redisHeaders
-    // })
-    // const result = await resp.json();
-    return await redis[isAdd ? 'incr' : 'decr'](key)
+    return await redis[isAdd ? "incr" : "decr"](key)
 }
 
 export async function getRedisVal(key: string) {
-    // const acceptValue = (await headers()).get('accept')
-    // console.log('acceptValue', acceptValue)
-    // const res = await fetch(`https://brief-kid-53738.upstash.io/get/${key}`, {
-    //     headers: redisHeaders,
-    //     // cache: 'no-store',
-    //     // next: { revalidate: 10 },
-    // })
-    // const { result } = await res.json()
-    // await sleep(500)
-    console.time('redis查询耗时')
+    console.time("redis查询耗时")
     const result = await redis.get<number>(key)
-    console.timeEnd('redis查询耗时')
+    console.timeEnd("redis查询耗时")
     return +(result || 0)
 }
 
-export async function getUserList(pageNumber = 1, pageSize = 5,) {
-    const startTime = Date.now()
-    const list = await sql(`SELECT * FROM users limit ${pageSize} offset ${pageSize * (pageNumber - 1)}`) as { id: string, name: string, email: string }[];
-    const t1 = Date.now() - startTime
-    const [{ count }] = await sql('select count(*) FROM users')
-    const t2 = Date.now() - startTime
-    console.log('sql query time', t1, t2,)
-    return { total: count, list, };
+export async function getUserList(pageNumber = 1, pageSize = 5) {
+    const [listResult, countResult] = await Promise.all([
+        sql(`SELECT *
+             FROM users LIMIT ${pageSize}
+             OFFSET ${pageSize * (pageNumber - 1)}`),
+        sql("SELECT COUNT(*) as count FROM users"),
+    ]);
+    // 提取结果
+    const list = listResult as { id: string, name: string, email: string }[];
+    const [{ count }] = countResult;
+    return { total: count, list };
 }
---
+++ ciScript.js
@@ -0,0 +1,2 @@
+fetch('https://blog.zhongke.icu/api/email').then(res => res.json()).then(console.log).catch(err => console.log(err));
+
